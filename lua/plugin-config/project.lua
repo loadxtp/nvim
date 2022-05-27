0f638b7ff65c2fd3a7e1d241b3f2cb77b28b9bf1
xieTP <xtp1994@gmail.com>
2022-05-27 16:06:10 +0800

    remove unused config

--- lua/plugin-config/project.lua
+++ lua/plugin-config/project.lua
@@ -11,21 +11,7 @@ if not status then
 end
 
 vim.g.nvim_tree_respect_buf_cwd = 1
-project.setup({
-  detection_methods = { "pattern" },
-  patterns = {
-    "README.md",
-    "Cargo.toml",
-    "package.json",
-    ".sln",
-    ".git",
-    "_darcs",
-    ".hg",
-    ".bzr",
-    ".svn",
-    "Makefile",
-  },
-})
+project.setup({})
 
 local status_telescope, telescope = pcall(require, "telescope")
 if not status_telescope then
